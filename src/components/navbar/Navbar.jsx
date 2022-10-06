4688113a9f3639173d1c55091db6bb38e0946a2a
ImMalith123 <110458554+ImMalith123@users.noreply.github.com>
2022-10-06 18:49:48 +1100

    Add files via upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './navbar.css';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div class="navbar-container">
-      <li><a class = "navbar-font" href="#">Home</a></li> 
-      <li><a class = "navbar-font" href="#">Course Diagram</a></li> 
-      <li><a class = "navbar-font" href="#">Useful Links </a></li> 
-
+      <li><a class = "navbar-font"> </a><Link to="/">Home</Link></li> 
+      <li><a class = "navbar-font"> </a><Link to="/main">Course Diagram</Link></li> 
+      <li><a class = "navbar-font">Useful Links </a></li> 
     </div>
 
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-
+import { BrowserRouter } from 'react-router-dom';
+import LandingPage from './LandingPage';
 
 import App from './App';
+import Main from './Main';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>, document.getElementById('root'));
